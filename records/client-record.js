d150064d15fc3f6a1da0dfd2977e72915c312449
Your Name <bvbrunik@gmail.com>
2023-02-27 17:15:20 +0100

    Making validation of during adding new customers

--- records/client-record.js
+++ records/client-record.js
@@ -1,6 +1,14 @@
 class ClientRecord {
 
     constructor(obj) {
+        const {name} = obj;
+        if(!name || typeof name !== 'string' || name.length < 3 ) {
+            throw new Error('The name must be a text of min. 3 characters.');
+        }
+
+        if(obj.name)
+
+        this.id = obj.id;
         this.name = obj.name;
         this.mail = obj.mail;
         this.nextContactAt = obj.nextContactAt;
@@ -10,5 +18,5 @@ class ClientRecord {
 }
 
 module.exports = {
-     ClientRecord,
+    ClientRecord,
 }
