29bd1ec8fbf7271457d024cfe9cadddb801a9355
Your Name <bvbrunik@gmail.com>
2023-02-27 17:23:20 +0100

    Added another validations of during adding new customers

--- records/client-record.js
+++ records/client-record.js
@@ -1,12 +1,25 @@
 class ClientRecord {
 
     constructor(obj) {
-        const {name} = obj;
-        if(!name || typeof name !== 'string' || name.length < 3 ) {
-            throw new Error('The name must be a text of min. 3 characters.');
+        const {id,name,mail,nextContactAt,notes} = obj;
+
+        if(!id || typeof id !== 'string' ) {
+            throw new Error('ID cannot be empty text.');
         }
 
-        if(obj.name)
+        if(!name || typeof name !== 'string' || name.length < 3 ) {
+            throw new Error('The name must be a text of min. 3 characters!');
+        }
+
+        if(!mail || typeof mail !== 'string' || mail.indexOf('@') === -1 ) {
+            throw new Error('Mail is incorrect!');
+        }
+        if (typeof nextContactAt !== 'string') {
+            throw new Error('Contact details must be text!')
+        }
+        if (typeof notes !== 'string') {
+            throw new Error('Notes must be text!')
+        }
 
         this.id = obj.id;
         this.name = obj.name;
